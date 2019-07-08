fac184a96ebceb2c4bce06fec12d11dc457d3b10
unknown <cmzolczynski@gmail.com>
2019-07-08 14:48:48 -0700

    routes

---
+++ app/routing/htmlroutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+
+module.exports = function(app) {
+
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./../public/home.html"));
+  });
+  
+  app.get("/control", function(req, res) {
+    res.sendFile(path.join(__dirname, "./../public/control.html"));
+  });
+
+};
