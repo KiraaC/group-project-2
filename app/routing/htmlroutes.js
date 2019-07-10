59f351c25b32cd724f9e9c5ab1e5e098514c0a47
Kira Corbin <madkatkeys@gmail.com>
2019-07-10 14:29:51 -0700

    updated js with mysql links

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -1,18 +1,26 @@
-var path = require("path");
+const path = require("path");
 
+module.exports = function (app) {
 
-module.exports = function(app) {
-
-// Routes
-app.get("/", function(req, res) {
+  // Routes
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./../public/home.html"));
   });
-  
-  app.get("/control", function(req, res) {
+
+  app.get("/control", function (req, res) {
     res.sendFile(path.join(__dirname, "./../public/control.html"));
   });
 
-  app.get("/contacts", function(req, res) {
+  // placeholder
+  app.get("/control", function (req, res) {
+    res.sendFile(path.join(__dirname, "./.././assets/mysql/cosplaydb"));
+  });
+
+  app.get("/control", function (req, res) {
+    res.sendFile(path.join(__dirname, "./.././assets/mysql/cosplaydb"));
+  });
+
+  app.get("/contacts", function (req, res) {
     res.sendFile(path.join(__dirname, "./../public/contacts.html"));
   });
 
