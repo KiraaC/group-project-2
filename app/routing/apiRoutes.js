ee22532c9c59d5f975c7a17d91b16f1d78ee4a41
unknown <cmzolczynski@gmail.com>
2019-07-11 10:50:07 -0700

    moved voicecontrol

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,18 @@
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Siryessir!1',
-    database: 'cosplaydb'
-})
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'Siryessir!1',
+//     database: 'cosplaydb'
+// })
 
-connection.connect(function (err) {
-    if (err) throw err
-    console.log('You are now connected...')
-})
+// connection.connect(function (err) {
+//     if (err) throw err
+//     console.log('You are now connected...')
+// })
 
-app.get("api/control", function (req, res) {
+// app.get("api/control", function (req, res) {
 
-    res.json(data)
+//     res.json(data)
 
-});
+// });
