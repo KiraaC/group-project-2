364c9e152475caf73a98fb1b133cfe1d92772d05
unknown <cmzolczynski@gmail.com>
2019-07-11 10:58:38 -0700

    voice controls

--- server.js
+++ server.js
@@ -12,27 +12,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('app/public'));
 
 require(path.join(__dirname, "./app/routing/htmlRoutes.js"))(app);
-// require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
+require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
 
-// require(path.join(__dirname, "./assets/mysql/cosplaydb"))(app);
-// require(path.join(__dirname, "./assets/mysql/cosplay_people"))(app);
+require(path.join(__dirname, "./assets/mysql/cosplaydb"))(app);
+require(path.join(__dirname, "./assets/mysql/cosplay_people"))(app);
 
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: 'Siryessir!1',
-//   database: 'cosplaydb'
-// })
-// connection.connect(function(err) {
-//   if (err) throw err
-//   console.log('You are now connected...')
-// })
-// connection.query('SELECT * FROM contacts', function(err, results) {
-//   if (err) throw err
-//   // console.log(results[0].first_name)
-//   // console.log(results[0].last_name)
-// })
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Siryessir!1',
+  database: 'cosplaydb'
+})
+connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')
+})
+connection.query('SELECT * FROM contacts', function(err, results) {
+  if (err) throw err
+  // console.log(results[0].first_name)
+  // console.log(results[0].last_name)
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
