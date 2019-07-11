acbf13d90e197f9d8240e1fb06e80198cb2e2fcb
Seyoung Yoon <sseang011@gmail.com>
2019-07-10 17:36:34 -0700

    fixed errors

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,18 +1,9 @@
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Siryessir!1',
-    database: 'cosplaydb'
-})
 
-connection.connect(function (err) {
-    if (err) throw err
-    console.log('You are now connected...')
-})
 
-app.get("api/control", function (req, res) {
 
-    res.json(data)
-
-});
+module.exports = function (app) {
+    // Routes
+    app.get("api/control", function (req, res) {
+        res.json(data)
+    });  
+  };
