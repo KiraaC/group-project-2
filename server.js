f2177fa2374df881905470b03d582a08ebe9a241
Kira Corbin <madkatkeys@gmail.com>
2019-07-15 15:41:50 -0700

    removed localhost data on server.js

--- server.js
+++ server.js
@@ -9,21 +9,6 @@ const PORT = process.env.PORT || 9040;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('app/public'));
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'cosplaydb'
-})
-
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack)
-  }
-  console.log('You are now connected...' + connection.threadId)
-})
-
 require(path.join(__dirname, "./app/routing/htmlRoutes.js"))(app);
 require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
 
