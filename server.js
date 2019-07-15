658fa9785405749bd6d6b3b4cdd651de41bd0cf7
Kira Corbin <madkatkeys@gmail.com>
2019-07-15 15:25:44 -0700

    final push

--- server.js
+++ server.js
@@ -4,27 +4,12 @@ const path = require("path");
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 9040;
-const mysql = require("mysql");
+
 
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('app/public'));
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Siryessir!1',
-  database: 'cosplaydb'
-})
-
-connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack)
-  }
-  console.log('You are now connected...' + connection.threadId)
-})
 
 require(path.join(__dirname, "./app/routing/htmlRoutes.js"))(app);
 require(path.join(__dirname, "./app/routing/apiRoutes.js"))(app);
